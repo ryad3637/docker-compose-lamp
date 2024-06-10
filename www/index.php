4cb4e9632f096b3129d78cae0006a8034d36c0fb
Ryad <mohamed-ryad.belhacene.1@ens.etsmtl.ca>
2024-06-10 15:02:56 -0400

    ajout php

--- www/index.php
+++ www/index.php
@@ -1,75 +1,155 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>LAMP STACK</title>
-        <link rel="shortcut icon" href="/assets/images/favicon.svg" type="image/svg+xml">
-        <link rel="stylesheet" href="/assets/css/bulma.min.css">
-    </head>
-    <body>
-        <section class="hero is-medium is-info is-bold">
-            <div class="hero-body">
-                <div class="container has-text-centered">
-                    <h1 class="title">
-                        LAMP STACK
-                    </h1>
-                    <h2 class="subtitle">
-                        Your local development environment
-                    </h2>
-                </div>
-            </div>
-        </section>
-        <section class="section">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Environment</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><?= apache_get_version(); ?></li>
-                                <li>PHP <?= phpversion(); ?></li>
-                                <li>
-                                    <?php
-                                    $link = mysqli_connect("database", "root", $_ENV['MYSQL_ROOT_PASSWORD'], null);
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body class="index">
 
-/* check connection */
-                                    if (mysqli_connect_errno()) {
-                                        printf("MySQL connecttion failed: %s", mysqli_connect_error());
-                                    } else {
-                                        /* print server version */
-                                        printf("MySQL Server %s", mysqli_get_server_info($link));
-                                    }
-                                    /* close connection */
-                                    mysqli_close($link);
-                                    ?>
-                                </li>
-                            </ul>
-                        </div>
+    <div class="container-index">
+
+        <div class="container-list">
+
+        <h1>Pokemon Type : </h1>
+            <ul class="type-list">
+                <li><a href="index.html">All</a></li>
+                <li><a href="index.html?type=flying">Flying</a></li>
+                <li><a href="index.html?type=fire">Fire</a></li>
+                <li><a href="index.html?type=water">Water</a></li>
+                <li><a href="index.html?type=grass">Grass</a></li>
+                <li><a href="index.html?type=ice">Ice</a></li>
+                <li><a href="index.html?type=normal">Normal</a></li>
+                <li><a href="index.html?type=dark">Dark</a></li>
+                <li><a href="index.html?type=electric">Electric</a></li>
+                <li><a href="index.html?type=fighting">Fighting</a></li>
+                <li><a href="index.html?type=poison">Poison</a></li>
+                <li><a href="index.html?type=ground">Ground</a></li>
+                <li><a href="index.html?type=rock">Rock</a></li>
+                <li><a href="index.html?type=psychic">Psychic</a></li>
+                <li><a href="index.html?type=bug">Bug</a></li>
+                <li><a href="index.html?type=ghost">Ghost</a></li>
+                <li><a href="index.html?type=steel">Steel</a></li>
+                <li><a href="index.html?type=dragon">Dragon</a></li>
+                <li><a href="index.html?type=fairy">Fairy</a></li>
+                <li><a href="index.html?type=Stellar">Stellar</a></li>
+                <li><a href="index.html?type=???">???</a></li>
+            </ul>
+        </div>
+        
+        <div class="container-pokemon">
+            <h2>Pokemon List</h2>
+            <ul class="pokemon-list">
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=493">Arceus</a> 
+                    <div class="img-indpkn">
+                    <img src="image/ARCEUS.png"
+                    alt="Arceus">
                     </div>
-                    <div class="column">
-                        <h3 class="title is-3 has-text-centered">Quick Links</h3>
-                        <hr>
-                        <div class="content">
-                            <ul>
-                                <li><a href="/phpinfo.php">phpinfo()</a></li>
-                                <li><a href="http://localhost:<? print $_ENV['PMA_PORT']; ?>">phpMyAdmin</a></li>
-                                <li><a href="/test_db.php">Test DB Connection with mysqli</a></li>
-                                <li><a href="/test_db_pdo.php">Test DB Connection with PDO</a></li>
-                            </ul>
-                        </div>
+                    <p>Types : Normal</p>
+                    <p>Génération : 4</p>
+                </li>
+            
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=646">Kyurem</a>
+                    <div class="img-indpkn">
+                    <img src="image/KYUREM.png"
+                    alt="Kyurem">
                     </div>
-                </div>
-            </div>
-        </section>
-        <footer class="footer">
-            <div class="content has-text-centered">
-                <p>
-                    <strong><a href="https://www.sprintcube.com" target="_blank">SprintCube</a></strong><br>
-                    The source code is released under the <a href="https://github.com/sprintcube/docker-compose-lamp/blob/master/LICENSE" target="_blank">MIT license</a>.
-                </p>
-            </div>
-        </footer>
-    </body>
+                    <p>Types : Dragon, Ice</p>
+                    <p>Generation : 5</p>
+                </li>
+                
+                
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=718">Zygarde</a>
+                    <div class="img-indpkn">
+                    <img src="image/ZYGARDE.png" 
+                    alt="Zygarde">
+                    </div>
+                    <p>Types : Dragon, Ground</p>
+                    <p>Generation: 6</p>
+                </li>
+                
+                
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=445">Garchomp</a>
+                    <div class="img-indpkn">
+                    <img src="image/CARCHAROCK.png"
+                    alt="Carchacrok">
+                    </div>
+                    <p>Types : Dragon, Ground</p>
+                    <p>Generation: 4</p>
+                </li>
+                
+                
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=448">Lucario</a>
+                    <div class="img-indpkn">
+                    <img src="image/LUCARIO.png"
+                    alt="Lucario">
+                    </div>
+                    <p>Types : Fighting, Steel</p>
+                    <p>Generation: 4</p>
+                </li>
+                
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=647">Keldeo</a>
+                    <div class="img-indpkn">
+                    <img src="image/KELDEO.png"
+                    alt="Keldeo">
+                    </div>
+                    <p>Types : Water, Fighting</p>
+                    <p>Generation: 5</p>
+                </li>
+                
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=149">Dragronite</a>
+                    <div class="img-indpkn">
+                    <img src="image/DRACOLOSSE.png" 
+                    alt="Dracolosse">
+                    </div>
+                    <p>Types : Dragon, Flying</p>
+                    <p>Generation: 2</p>
+                </li>
+                
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=658">Greninja</a>
+                    <div class="img-indpkn">
+                    <img src="image/AMPHINOBI.png"
+                    alt="Amphinobi">
+                    </div>
+                    <p>Types : Water, Dark </p>
+                    <p>Generation: 6</p>
+                </li>
+                
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=251">Celebi</a>
+                    <div class="img-indpkn">
+                    <img src="image/CELEBI.png"
+                    alt="Celebi">
+                    </div>
+                    <p>Types : Psychic, Grass</p>
+                    <p>Generation: 2</p>
+                </li>
+                
+
+                <li class="pokemon-item"><a class="pokemon-link" href="pokemon.html?no=644">Zekrom</a>
+                    <div class="img-indpkn">
+                    <img src="image/ZEKROM.png"
+                    alt="Zekrom">
+                    </div>
+                    <p>Types : Dragon, Electric</p>
+                    <p>Generation: 5</p>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+    <div class="index-button" >
+        <a href="form.html">Pokemon Form</a>
+    </div>
+ 
+    <script src="index.js"></script>
+    
+</body>
 </html>
